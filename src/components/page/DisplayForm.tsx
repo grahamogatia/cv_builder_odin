c650c08d95b46a436ac9d74c553211b346c3c60c
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-09-17 09:18:12 +0800

    Add list of tasks to DisplayForm for CV functionality

--- src/components/page/DisplayForm.tsx
+++ src/components/page/DisplayForm.tsx
@@ -6,6 +6,12 @@ function DisplayForm() {
     >
       <h1 className="text-xl font-bold">My A4 Document</h1>
       <p>This simulates a real A4 page.</p>
+      <ul className="text-red-800">
+        <li>Fix Save Functionality</li>
+        <li>Decide how to store CV details</li>
+        <li>Implement details to Harvard Template</li>
+        <li>Implement download PDF button</li>
+      </ul>
     </div>
   );
 }
